e2f3269da6e373aba041db9cbe781811a416a329
Masoumeh-Hashemi <masoumeh.hashemi92@gmail.com>
2024-06-01 22:54:12 +0330

    :sparkles: add command handler RemoveTodoListFromUserHandler

---
+++ src/application/user/command-handlers/remove-todo-list-from-user.handler.ts
@@ -0,0 +1,16 @@
+import { Inject } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { RemoveTodoListFromUserCommand } from '../commands/remove-todo-list-from-user.command';
+import { UserService } from '../services/user.service';
+
+@CommandHandler(RemoveTodoListFromUserCommand)
+export class RemoveTodoListFromUserHandler
+  implements ICommandHandler<RemoveTodoListFromUserCommand>
+{
+  constructor(private readonly userService: UserService) {}
+
+  async execute(command: RemoveTodoListFromUserCommand): Promise<void> {
+    const { userId, listId } = command;
+    const user = await this.userService.removeTodoListFromUser(userId, listId);
+  }
+}
