58f664fe9d0eefa378796d70860e6b61107695c8
Masoumeh-Hashemi <masoumeh.hashemi92@gmail.com>
2024-06-01 22:52:12 +0330

    :sparkles: add event handlers

---
+++ src/application/todo/event-handlers/todo-list.event-handlers.ts
@@ -0,0 +1,12 @@
+// application/todo/event-handlers/todo-list.event-handlers.ts
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { TodoListCreatedEvent } from '../../../domain/todo/events/todo-list-created.event';
+
+@EventsHandler(TodoListCreatedEvent)
+export class TodoListCreatedEventHandler
+  implements IEventHandler<TodoListCreatedEvent>
+{
+  handle(event: TodoListCreatedEvent) {
+    console.log(`Todo list created: ${event.title}`);
+  }
+}
---
+++ src/application/user/event-handlers/user.event-handlers.ts
@@ -0,0 +1,13 @@
+// application/user/event-handlers/user.event-handlers.ts
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { UserRegisteredEvent } from '../../../domain/user/events/user-registered.event';
+
+@EventsHandler(UserRegisteredEvent)
+export class UserRegisteredEventHandler
+  implements IEventHandler<UserRegisteredEvent>
+{
+  handle(event: UserRegisteredEvent) {
+    //Can be used for any further notification or email
+    console.log(`User registered: ${event.username}`);
+  }
+}
